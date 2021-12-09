74d71a437f99e496f3042a07ac3be1c57430fe75
Ed Fitzgerald <edmund.fitzgerald@emydex.com>
2021-12-09 10:09:06 +0000

    day one

---
+++ aoc_day1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+
+namespace aoc_day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Program = new Program();
+            var depths = Program.ReadDepthsFromFile();
+
+            Console.WriteLine("simple increases - " + Program.CountIncreasesInDepth(depths));
+            Console.WriteLine("window increases - " + Program.CountIncreasesInDepthSlidingWindow(depths));
+        }
+
+        private int[] ReadDepthsFromFile()
+        {
+            string[] depthsAsStrings = File.ReadAllLines("c:\\ed\\depths.txt");
+            
+            // lets convert it to numbers now, once.
+            int[] depths = new int[depthsAsStrings.Length];
+
+            for (int i = 0; i < depths.Length; i++)
+            {
+                depths[i] = int.Parse(depthsAsStrings[i]);
+            }
+            return depths;
+        }
+
+        private int CountIncreasesInDepth(int[] depths)
+        {
+            int count = 0;
+
+            for (int i =0; i< depths.Length -1; i++)
+            {
+                if (depths[i + 1] > depths[i]) count++; 
+            }
+
+            return count;
+        }
+
+        private int CountIncreasesInDepthSlidingWindow(int[] depths)
+        {
+            int count = 0;
+
+            for (int i = 0; i < depths.Length - 3; i++)
+            {
+                if (SumOfWindow(depths,i + 1) > SumOfWindow(depths, i)) count++;
+            }
+
+            return count;
+        }
+
+        private int SumOfWindow(int[]depths, int position)
+        {
+            return depths[position] + depths[position + 1] + depths[position + 2];
+        }
+    }
+}
