ee74e6b7f051a3771794ac6dc6efd65ba4dc74c2
Edmund Fitzgerald <edmundf32@gmail.com>
2021-12-12 15:01:29 +0000

    begin Day7

---
+++ Aoc_days/Day7.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aoc_days
+{
+     class Day7
+    {
+        Helper helper = new Helper();
+        public void Answer()
+        {
+
+            Console.WriteLine("Day 6!");
+            var file = helper.ReadString("day6Input.txt")[0].Split(",");
+        }
+
+        }
+}
