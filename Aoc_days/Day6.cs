120d224c3ec3893ab41df27baafb0f1cb6afb1f6
Edmund Fitzgerald <edmundf32@gmail.com>
2021-12-12 11:19:55 +0000

    day 6 complete

--- Aoc_days/Day6.cs
+++ Aoc_days/Day6.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,36 +10,63 @@ namespace Aoc_days
     class Day6
     {
         Helper helper = new Helper();
-        List<int> NewFish = new List<int>();
+    //    List<int> NewFish = new List<int>();
         public void Answer()
         {
 
             Console.WriteLine("Day 6!");
             var file = helper.ReadString("day6Input.txt")[0].Split(",");
 
-            List<int> AllTheFishes = new List<int>();
+            List<BigInteger> AllTheFishes = new List<BigInteger>() {0,0,0,0,0,0,0,0,0};
 
             //get the data
 
-            foreach(var item in file)
+            foreach (var item in file)
             {
-                AllTheFishes.Add(Convert.ToInt32(item));
+                int daysOld = Convert.ToInt32(item);
+
+                AllTheFishes[daysOld] ++;
             }
 
 
 
             // do the days
-            Console.WriteLine("Initial State: " + PrintFishes(AllTheFishes));
+            Console.WriteLine("Initial State: " );
             for (int i = 1; i < 257; i++)
             {
                 AllTheFishes = BreedFishes(AllTheFishes);
-                Console.WriteLine("Number of fishes " + AllTheFishes.Count() + " After " + i + " days: ");// + PrintFishes(AllTheFishes));
+                
             }
-
+            Console.WriteLine("the Number of fishes " + GetAllTheFishies(AllTheFishes));// + PrintFishes(AllTheFishes));
         }
 
-        private List<int> BreedFishes(List<int> AllTheFishes)
+        private BigInteger GetAllTheFishies(List<BigInteger> theFishes)
         {
+            BigInteger returnFishes = 0;
+            foreach(BigInteger i in theFishes)
+            {
+                returnFishes += i;
+            }
+
+            return returnFishes;
+        }
+
+        private List<BigInteger> BreedFishes(List<BigInteger> AllTheFishes)
+        {
+            BigInteger newfishes = AllTheFishes[0];
+
+            for (int i = 0; i < 8; i++ )
+            {
+                if(i==6) { AllTheFishes[i] = AllTheFishes[i + 1] + newfishes; }
+                else { AllTheFishes[i] = AllTheFishes[i + 1]; }         
+            }
+
+
+
+            AllTheFishes[8] = newfishes;
+
+            return AllTheFishes;
+            /*
             int numberOfFishes = AllTheFishes.Count();
 
             for (int i = 0; i < numberOfFishes; i++)
@@ -48,18 +76,6 @@ namespace Aoc_days
                     AllTheFishes[i] = AllTheFishes[i] + 7;
                     AllTheFishes.Add(8);
 
-                    /*
-                    if (NewFish.Contains(i))
-                    {
-                        AllTheFishes.Add(8);
-                        AllTheFishes[i] = 7;
-                    }
-                    else
-                    {
-                        AllTheFishes.Add(8);
-                        NewFish.Add(i);
-                    }   
-                    */
                 }
                 AllTheFishes[i] = AllTheFishes[i] - 1;
             }
@@ -76,6 +92,8 @@ namespace Aoc_days
             }
 
             return returnString.Remove(returnString.Length - 1, 1);
+        }
+            */
         }
     }
 }
