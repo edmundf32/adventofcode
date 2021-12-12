09eff47ac6929fd15137c5b78931236b20977722
Edmund Fitzgerald <edmundf32@gmail.com>
2021-12-12 15:23:48 +0000

    day 7 part one

--- Aoc_days/Day7.cs
+++ Aoc_days/Day7.cs
@@ -13,8 +13,38 @@ namespace Aoc_days
         {
 
             Console.WriteLine("Day 6!");
-            var file = helper.ReadString("day6Input.txt")[0].Split(",");
+            var file = helper.ReadString("day7Input.txt")[0].Split(",");
+            List<int> positions = new List<int>();
+
+            foreach (var item in file)
+            {
+                positions.Add(int.Parse(item));
+            }
+
+            var maxNum = positions.Max(z => z);
+
+            int totalFuel = maxNum * positions.Count();
+
+            for (int i = 0; i < maxNum; i ++)
+            {
+                int guessFuel = 0;
+
+                foreach ( int item in positions)
+                {
+                    guessFuel += Math.Abs(item-i);
+                }
+                Console.WriteLine("iteration - " + i + "guess " + guessFuel);
+                if (guessFuel < totalFuel) totalFuel = guessFuel;
+
+
+            }
+            Console.WriteLine(" minimum " + totalFuel);
         }
 
-        }
+        
+
+
+
+
+    }
 }
